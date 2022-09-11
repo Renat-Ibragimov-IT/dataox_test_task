5cdd77d123e8d399bf76459c91010bff9efee9b5
renat-ibragimov-it <renatibragimov26@gmail.com>
2022-09-12 01:16:11 +0300

    Postgress connector refactored

--- postgres/postgres_connector.py
+++ postgres/postgres_connector.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql+psycopg2://"
-                       "postgres:260592@localhost/test_db")
+engine = create_engine("postgresql+psycopg2://dataox:dataox"
+                       "@localhost:5433/dataox_db")
 
 Session = sessionmaker(engine, expire_on_commit=False)
