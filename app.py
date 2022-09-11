e3fb549b699f2b0b39650543f7ad25d2f6759399
renat-ibragimov-it <renatibragimov26@gmail.com>
2022-09-11 22:51:18 +0300

    Main file refactored

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from data_collectors.async_data_collector import AsyncDataCollector
 from data_collectors.sync_data_collector import SyncDataCollector
 
 
-def run_parser(parser_type: str):
+def run_parser(parser_type: str, page_from: int, page_to: int, save_to: str):
     parser_choices = {"sync": SyncDataCollector,
                       "async": AsyncDataCollector}
-    return parser_choices[parser_type](args.page_from, args.page_to,
-                                       args.save_to)
+    return parser_choices[parser_type](page_from, page_to,
+                                       save_to)
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description="Parser app")
     parser.add_argument('page_from', default=1, type=int,
                         help="Specify the site page to start parsing")
@@ -25,4 +25,9 @@ if __name__ == "__main__":
                         help="Specify which type of parsing to use")
     args = parser.parse_args()
 
-    run_parser(args.parser_type)
+    run_parser(args.parser_type, args.page_from, args.page_to,
+               args.save_to)
+
+
+if __name__ == "__main__":
+    main()
