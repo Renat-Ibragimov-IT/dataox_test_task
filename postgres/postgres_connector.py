74da736793441dca74a0df0c3af92dea4aaa5414
renat-ibragimov-it <renatibragimov26@gmail.com>
2022-09-11 22:51:53 +0300

    Postgres connector refactored

--- postgres/postgres_connector.py
+++ postgres/postgres_connector.py
@@ -3,6 +3,5 @@ from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("postgresql+psycopg2://"
                        "postgres:260592@localhost/test_db")
-engine.connect()
 
 Session = sessionmaker(engine, expire_on_commit=False)
